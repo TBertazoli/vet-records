f838a5409d69f19dd29d05306bc546bfbda69baa
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-04-09 00:16:10 -0500

    added method override

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const methodOverride = require("method-override");
 const session = require("express-session");
 const passport = require("passport");
 
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
 //session
 app.use(
